157b17a876c57bcada56b81e64e9bfcd261833b1
howsilence <statton0104@gmail.com>
2021-12-10 11:56:35 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.querySelector('#main');
+const body = document.querySelector('body');
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Steve is the champion"
+const h1 = document.getElementById("victory");
+
+
+body.appendChild(newHeader);
+
